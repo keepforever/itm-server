590bcc8be45d9c8cdbec63107347ab13ef810f03
Brian Cilenti <briancilenti@gmail.com>
2018-07-12 22:22:11 -0700

    added TODO comments for my next move

--- src/resolvers/Mutation/offer.js
+++ src/resolvers/Mutation/offer.js
@@ -5,8 +5,11 @@ const offer = {
   deleteOffer: forwardTo("db"),
   updateOffer: forwardTo("db"),
   createOffer: (parent, args, ctx, info) => {
-    //getUserId makes sure sender of requset has a valid token
-    //in the HTTP Headers thereby protecting the route
+    // TODO, add logic to check that Seller has an availble offer allowance 
+    // before creating offer and connecting offer to user's inbox and sellers offers
+
+    // getUserId makes sure sender of requset has a valid token 
+    // in the HTTP Headers thereby protecting the route
     getUserId(ctx)
     return forwardTo("db")(parent, args, ctx, info);
   },
